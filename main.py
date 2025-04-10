9f20db8decfda9c0b6be3e0b569ab7a1f2e2da95
BERGER <jeanchristophe.berger@highridgemed.com>
2025-04-10 14:24:43 +0200

    Initial commit avec fichier main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Assistant Pro Démo", page_icon="🤖", layout="wide")
+
+st.title("Assistant Pro")
+st.write("Bienvenue dans votre assistant personnalisé !")
